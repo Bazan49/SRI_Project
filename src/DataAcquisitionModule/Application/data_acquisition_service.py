79e0b9b78d112ba624e894b23191b3255c130b6c
Bazan49 <aixabazan0705@gmail.com>
2026-03-31 13:09:27 -0400

    fix(DataAcquisitionService): add batch size param

--- src/DataAcquisitionModule/Application/data_acquisition_service.py
+++ src/DataAcquisitionModule/Application/data_acquisition_service.py
@@ -3,11 +3,11 @@ from DataAcquisitionModule.Infrastructure.scraper.scraper_factory import Scraper
 from DataAcquisitionModule.Infrastructure.storage.jsonl_storage_repository import JSONLRepository
 
 class DataAcquisitionService:
-    def __init__(self, max_pages=50, max_depth=3, delay=1):
+    def __init__(self, max_pages=50, max_depth=3, delay=1, batch_size=10):
 
         self.max_pages = max_pages
         self.crawler = Crawler(max_depth=max_depth, delay=delay)
-        self.repository = JSONLRepository(path="data/initial_corpus.jsonl", batch_size=10)
+        self.repository = JSONLRepository(path="data/initial_corpus.jsonl", batch_size=batch_size)
 
     def run(self):
 
@@ -15,8 +15,9 @@ class DataAcquisitionService:
             try:
                 scraper = ScraperFactory.get_scraper(url)
                 document = scraper.extract(url, html)
-                self.repository.save(document)
-                print(f"[OK] Document extracted and saved: {url}")
+                if document:
+                    self.repository.save(document)
+                    print(f"[OK] Document extracted and saved: {url}")
             except Exception as e:
                 print(f"[ERROR] Failed to process {url}: {e}")
 
